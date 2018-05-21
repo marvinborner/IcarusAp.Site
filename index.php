8ac3d52908b38e76a5a4d0b0cd1a9b508f2c55a3
Marvin Borner <marvin-borner@live.com>
2018-05-21 19:37:47 +0200

    Rewritten most things, added animations

--- index.php
+++ index.php
@@ -1,32 +1,43 @@
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aakash NP</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" />
-    <link rel="stylesheet" href="assets/css/index.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="assets/css/main.css" />
     <link rel="shortcut icon" type="image/x-icon" href="./assets/images/OwnAvatar_02.png">
+
+    <title>Aakash NP</title>
 </head>
-
 <body>
-<div class="top">
 
-</div>
-<div class="title">
-    <img src="./assets/images/OwnAvatar_02.png" width="100px" height="100px" alt="AAKASN NP"></img>
-    <h1>AAKASH NP</h1>
-</div>
-<div class="about">
-    <ul>
-        <li>20 years old</li>
-        <li>Tech Enthusiast</li>
-        <li>Smartphone Photographer</li>
-        <li>Vector Artist</li>
-        <li>Digital Sketcher</li>
-        <li>Computer Engineer</li>
-        <li>Founder of AndroUnity</li>
-    </ul>
+<i class="LoadingAnimation fas fa-circle-notch fa-spin" style="font-size:40px"></i>
+
+<div class="Main">
+    <img class="TopImage" src="assets/images/bg.jpg" />
+    <div class="Header">
+        <img src="./assets/images/OwnAvatar_02.png" width="100px" height="100px" alt="Aakash NP" />
+        <h1>Aakash NP</h1>
+    </div>
+
+    <div class="AboutText">
+        <ul>
+            <li>20 years old</li>
+            <li>Tech Enthusiast</li>
+            <li>Smartphone Photographer</li>
+            <li>Vector Artist</li>
+            <li>Digital Sketcher</li>
+            <li>Computer Engineer</li>
+            <li>Founder of AndroUnity</li>
+        </ul>
+    </div>
 </div>
 
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://use.fontawesome.com/releases/v5.0.13/js/all.js"></script>
+<script src="assets/js/main.js"></script>
 
 </body>
 </html>
