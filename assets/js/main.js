912b88a81a28981e8f43c2b08104e6a875ef27ae
Marvin Borner <marvin-borner@live.com>
2018-05-21 23:32:18 +0200

    Added sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,4 @@
-$(document).ready(function () {
+$(window).bind("load", function() {
     $(".LoadingAnimation").fadeOut();
     $(".Main").fadeIn();
 });
-
-function GoToSite(SiteName) {
-    switch (SiteName) {
-        case "Twitter":
-            window.location="https://twitter.com/IcarusAP29";
-            break;
-        case "Instagram":
-            window.location="https://www.instagram.com/paranoidphotographer.ig/";
-            break;
-        case "YouTube":
-            window.location="https://www.youtube.com/channel/UC2wTAKmO8zdEyxVJJoNdePQ";
-            break;
-        case "GooglePlus":
-            window.location="https://plus.google.com/+AakashNPAAKASHNP";
-            break;
-    }
-}
--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     <img class="TopImage" src="assets/images/bg.jpg" />
     <div class="Header">
         <img src="./assets/images/OwnAvatar_02.png" alt="Aakash NP" />
-        <h1>Aakash NP</h1>
+        <div class="TitleWrapper"><h1>Aakash NP</h1></div>
     </div>
 
     <div class="AboutText">
@@ -35,11 +35,60 @@
     </div>
 
     <div class="SocialMedia">
-        <i class="SocialFab Twitter fab fa-twitter" onclick="GoToSite('Twitter')"></i>
-        <i class="SocialFab Instagram fab fa-instagram" onclick="GoToSite('Instagram')"></i>
-        <i class="SocialFab YouTube fab fa-youtube" onclick="GoToSite('YouTube')"></i>
-        <i class="SocialFab GooglePlus fab fa-google-plus-g" onclick="GoToSite('SocialFab')"></i>
+        <a href="https://twitter.com/IcarusAP29" target="_blank"><i
+                    class="SocialFab Twitter fab fa-twitter" data-fa-transform="shrink-8"></i></a>
+        <a href="https://www.instagram.com/paranoidphotographer.ig/" target="_blank"><i
+                    class="SocialFab Instagram fab fa-instagram" data-fa-transform="shrink-8"></i></a>
+        <a href="https://www.youtube.com/channel/UC2wTAKmO8zdEyxVJJoNdePQ" target="_blank"><i
+                    class="SocialFab YouTube fab fa-youtube" data-fa-transform="shrink-8"></i></a>
+        <a href="https://plus.google.com/+AakashNPAAKASHNP" target="_blank"><i
+                    class="SocialFab GooglePlus fab fa-google-plus-g" data-fa-transform="shrink-8"></i></a>
     </div>
+
+    <div class="TitleWrapper">
+        <h2 class="TopMargin">My Work</h2>
+        <div class="SubTitle">Type 1 to type n</div>
+    </div>
+
+    <div class="MyWork ImageGrid">
+        <img src="assets/images/Work/PopStyle/Pop_Art_AakashNP.png" alt="Pop art image of me">
+        <img src="assets/images/Work/VectorAvatars/Tharun.png" alt="Vector avatar of Tharun">
+        <img src="assets/images/Work/PopStyle/Pop_ART_AakashNP_2.png" alt="Pop art image of me">
+        <img src="assets/images/Work/VectorAvatars/Marvin.png" alt="Vector avatar of Marvin">
+        <img src="assets/images/Work/PopStyle/POP_ART_HARSHV23.png" alt="Pop art image of HarshV23">
+        <img src="assets/images/Work/PopStyle/POP_ART_Keila.png" alt="Pop art image of Keila">
+        <img src="assets/images/Work/VectorPortraits/Hugh%20Jackman.png" alt="Vector portrait of Hugh Jackman">
+        <img src="assets/images/Work/PopStyle/Pop_Art_Rohit_kumar.png" alt="Pop art image of Rohit">
+        <img src="assets/images/Work/PopStyle/Till_Pop_Art.png" alt="Pop art image of Till">
+        <img src="assets/images/Work/VectorPortraits/Jon.png" alt="Vector portrait of Jon">
+    </div>
+
+    <div class="TitleWrapper">
+        <h2>Contact me</h2>
+    </div>
+
+    <div class="ContactMe">
+        <a href="javascript:void(0)" target="_blank"><i class="ContactFab Hangouts fas fa-quote-right"
+                                                        data-fa-transform="shrink-8"></i></a>
+        <a href="javascript:void(0)" target="_blank"><i class="ContactFab PaperPlane fas fa-paper-plane"
+                                                        data-fa-transform="shrink-8"></i></a>
+        <a href="javascript:void(0)" target="_blank"><i class="ContactFab gMail fas fa-envelope"
+                                                        data-fa-transform="shrink-8"></i></a>
+    </div>
+
+    <div class="SuperCoolLifeMotto">
+        <div class="QuoteColumn">
+            <div class="fas fa-quote-left" data-fa-transform="grow-10"></div>
+        </div>
+        <div class="TextColumn">
+            Focus on your life like a vector, Never get pixelated <span class="QuoteAuthor">Aakash NP</span>
+        </div>
+    </div>
+
+    <div class="ScrollUp" onclick="$('html, body').animate({ scrollTop: 0 }, 'fast')"><i class="ScrollFab fas fa-chevron-up" data-fa-transform="shrink-8"></i></div>
+
+    <div class="Credits">Created and hosted with <i class="fas fa-heart"></i> by <a href="https://marvinborner.me" target="_blank">Marvin Borner</a> and <a href="https://marcoalexanderfischer.de" target="_blank">Marco Alexander Fischer</a></div>
+
 </div>
 
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
