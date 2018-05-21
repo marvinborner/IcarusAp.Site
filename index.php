2ce8cdbb0b73374866d6e0c6c2033063f1625970
Marvin Borner <marvin-borner@live.com>
2018-05-21 18:31:25 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aakash NP</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css"/>
+    <link rel="stylesheet" href="assets/css/index.css"/>
+	<link rel="shortcut icon" type="image/x-icon" href="./assets/images/OwnAvatar_02.png">
+</head>
+
+<body>
+<div class="top">
+
+</div>
+<div class="title">
+<img src="./assets/images/OwnAvatar_02.png" width="100px" height="100px" alt="AAKASN NP"></img>
+<h1>AAKASH NP</h1>
+</div>
+<div class="about">
+<p>20 years old | Tech Ethusiast | Smartphone Photograper | Vector Artist | Digital Sketcher | Computer Engineer | Founder of AndroUnity</p>
+</div>
+
+
+</body>
+</html>
