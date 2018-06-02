982d4b886709b9ae9209081a29c523340d0c0442
Marvin Borner <marvin-borner@live.com>
2018-06-02 14:54:31 +0200

    Almost complete rewrite, many design changes

--- index.php
+++ index.php
@@ -7,91 +7,54 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link rel="stylesheet" href="assets/css/main.css" />
-    <link rel="shortcut icon" type="image/x-icon" href="./assets/images/OwnAvatar_02.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/OwnAvatar_02.png">
 
     <title>Aakash NP</title>
 </head>
 <body>
 
 <div class="Main">
-    <img class="TopImage" src="assets/images/bg.jpg" />
+    <img class="TopLeftLogo" src="assets/images/Logo.png">
+
+    <img class="TopImage" src="assets/images/background_small.png" />
     <div class="Header">
-        <h2 class="TopMargin">Aakash NP</h2>
-        <img src="./assets/images/OwnAvatar_02.png" alt="Aakash NP" />
+        <img src="assets/images/Aakash_avatar.png" alt="Aakash NP" />
+        <div class="NameWrapper">Aakash NP</div>
     </div>
 
-    <div class="AboutText">
+    <div class="AboutOverview">
         <ul>
-            <li>20 years old</li>
+            <li>Android Customizer</li>
             <li>Tech Enthusiast</li>
-            <li>Smartphone Photographer</li>
-            <li>Vector Artist</li>
-            <li>Digital Sketcher</li>
-            <li>Computer Engineer</li>
-            <li>Founder of AndroUnity</li>
+            <li>Graphic Artist</li>
+            <li>AndroUnity Founder</li>
         </ul>
     </div>
 
-    <div class="SocialMedia">
-        <a href="https://twitter.com/IcarusAP29" target="_blank"><i
-                    class="SocialFab Twitter fab fa-twitter" data-fa-transform="shrink-8"></i></a>
-        <a href="https://www.instagram.com/paranoidphotographer.ig/" target="_blank"><i
-                    class="SocialFab Instagram fab fa-instagram" data-fa-transform="shrink-8"></i></a>
-        <a href="https://www.youtube.com/channel/UC2wTAKmO8zdEyxVJJoNdePQ" target="_blank"><i
-                    class="SocialFab YouTube fab fa-youtube" data-fa-transform="shrink-8"></i></a>
-        <a href="https://plus.google.com/+AakashNPAAKASHNP" target="_blank"><i
-                    class="SocialFab GooglePlus fab fa-google-plus-g" data-fa-transform="shrink-8"></i></a>
-    </div>
-
-    <div class="TitleWrapper">
-        <h2 class="TopMargin">My Work</h2>
-        <div class="SubTitle">Type 1 to type n</div>
-    </div>
-
-    <div class="MyWork ImageGrid">
-        <img src="assets/images/Work/PopStyle/Pop_Art_AakashNP.png" alt="Pop art image of me">
-        <img src="assets/images/Work/VectorAvatars/Tharun.png" alt="Vector avatar of Tharun">
-        <img src="assets/images/Work/PopStyle/Pop_ART_AakashNP_2.png" alt="Pop art image of me">
-        <img src="assets/images/Work/VectorAvatars/Marvin.png" alt="Vector avatar of Marvin">
-        <img src="assets/images/Work/PopStyle/POP_ART_HARSHV23.png" alt="Pop art image of HarshV23">
-        <img src="assets/images/Work/PopStyle/POP_ART_Keila.png" alt="Pop art image of Keila">
-        <img src="assets/images/Work/VectorPortraits/Hugh%20Jackman.png" alt="Vector portrait of Hugh Jackman">
-        <img src="assets/images/Work/PopStyle/Pop_Art_Rohit_kumar.png" alt="Pop art image of Rohit">
-        <img src="assets/images/Work/PopStyle/Till_Pop_Art.png" alt="Pop art image of Till">
-        <img src="assets/images/Work/VectorPortraits/Jon.png" alt="Vector portrait of Jon">
-    </div>
-
-    <div class="TitleWrapper">
-        <h2>Contact me</h2>
-    </div>
-
-    <div class="ContactMe">
-        <a href="javascript:void(0)" target="_blank"><i class="ContactFab Hangouts fas fa-quote-right"
-                                                        data-fa-transform="shrink-8"></i></a>
-        <a href="https://t.me/IcarusAP" target="_blank"><i class="ContactFab Telegram fab fa-telegram-plane"
-                                                        data-fa-transform="shrink-8"></i></a>
-        <a href="mailto:aakash19.98pawar@gmail.com" target="_blank"><i class="ContactFab gMail fas fa-envelope"
-                                                        data-fa-transform="shrink-8"></i></a>
-    </div>
-
-    <div class="SuperCoolLifeMotto">
-        <div class="QuoteColumn">
-            <div class="fas fa-quote-left" data-fa-transform="grow-10"></div>
-        </div>
-        <div class="TextColumn">
-            Focus on your life like a vector, Never get pixelated <span class="QuoteAuthor">Aakash NP</span>
+    <hr class="AboutDivider">
+    <div class="AboutSection">
+        <div class="SubTitle">About me</div>
+        <div class="AboutText">
+            Hello World! I'm a young Graphic Artist from India improving my skills step by step.
+            I make Vector Artworks, Digital Sketches and Videos. I make different types of Vector Artworks like:
         </div>
     </div>
 
-    <div class="ScrollUp" onclick="$('html, body').animate({ scrollTop: 0 }, 'fast')"><i class="ScrollFab fas fa-chevron-up" data-fa-transform="shrink-8"></i></div>
+    <div class="MyWork">
+        <div class="ImageGrid">
+            <img class="Work" src="assets/images/Rohit_pop_art.png" alt="Rohit">
+            <div class="ImageSubCaption">ElectroPOP Vector Avatars</div>
+            <div class="ImageSubText"></div>
+        </div>
+    </div>
 
     <div class="Credits">Created and hosted with <i class="fas fa-heart"></i> by <a href="https://marvinborner.me" target="_blank">Marvin Borner</a> and <a href="https://marcoalexanderfischer.de" target="_blank">Marco Alexander Fischer</a></div>
 
 </div>
 
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src="https://use.fontawesome.com/releases/v5.0.13/js/all.js"></script>
-<script src="assets/js/main.js"></script>
+<!--<script src="https://use.fontawesome.com/releases/v5.0.13/js/all.js"></script>
+<script src="assets/js/main.js"></script>-->
 
 </body>
 </html>
